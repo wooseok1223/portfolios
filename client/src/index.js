23975f8114c8c59e9e92fdf8aaa4b985b2c02a8d
tjdntjr123 <tjdntjr123@douzone.com>
2020-08-17 16:51:42 +0900

    샘플 헤더 추가

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App ,{Home, First} from './App'
+// import { Router, Route, browserHistory,IndexRoute } from 'react-router';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +10,13 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// ReactDOM.render(
+//   <Router history={browserHistory}>
+//     <Route path ="/" component={App}>
+//         <IndexRoute component={Home}/>
+//         <Route path="first" component={First}/>
+//     </Route>
+//   </Router>,
+//   document.getElementById('root')
+// );
+
---
+++ client/src/page/MainPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../contents/Header'
+
+function MainPage() {
+    return (
+        <div>
+            <Header/>
+        </div>
+    );
+}
+
+
+export default MainPage;
